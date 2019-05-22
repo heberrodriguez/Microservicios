81b84382f8e1434d5e81c29dd36433a9e9286391
heberrodriguez <heber.rodriguez@seidor.cl>
2019-05-22 16:41:45 -0400

    tes7777

---
+++ ms-chilecompra-documentos2/src/main/java/gov/cl/chilecompra/registroproveedor/documentos/MsChilecompraDocumentos2Application.java
@@ -0,0 +1,13 @@
+package gov.cl.chilecompra.registroproveedor.documentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChilecompraDocumentos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsChilecompraDocumentos2Application.class, args);
+	}
+
+}
